06f4ecf823575a06bb58c1fccc215ad0d7010d5e
JoseMatos03 <joser.fmatos03@gmail.com>
2023-04-17 11:40:47 +0100

    test(artigos): testes em sapatilhas

--- src/test/artigos/sapatilhas/SapatilhasTest.java
+++ src/test/artigos/sapatilhas/SapatilhasTest.java
@@ -5,12 +5,46 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 import vintage.artigos.sapatilhas.Sapatilhas;
+import vintage.transportadoras.Transportadora;
+
 import static vintage.artigos.sapatilhas.Sapatilhas.ATACADORES;
-import static vintage.artigos.sapatilhas.Sapatilhas.ATILHOS;;
+import static vintage.artigos.sapatilhas.Sapatilhas.ATILHOS;
 
 public class SapatilhasTest {
 
-    // Sapatilhas-Tipo a testar
+    // Transportadoras
+    Transportadora transportadoraTeste_um = new Transportadora(5.00f);
+    Transportadora transportadoraTeste_dois = new Transportadora(2.00f, 1.20f);
 
-    
+    // Sapatilhas-Tipo a testar
+    private Sapatilhas sapatilhasTeste_um = new Sapatilhas(
+            0.85f,
+            2,
+            "Sapatilhas",
+            "Adidas",
+            55.25f,
+            39,
+            ATACADORES,
+            "Branco",
+            2022,
+            transportadoraTeste_dois);
+
+    private Sapatilhas sapatilhasTeste_dois = new Sapatilhas(
+            0.65f,
+            5,
+            "Sapatilhas",
+            "Nike",
+            26.00f,
+            35,
+            ATILHOS,
+            "Preto",
+            2018,
+            transportadoraTeste_um);
+
+    @Test
+    public void dadoPrecoBaseEPrecoCorrecao_quandoCalcularPreco_retornaPrecoFinal() {
+        float resultadoTeste_um = sapatilhasTeste_um.calcularPreco();
+        assertEquals(46.96f, resultadoTeste_um, 0.01f);
+
+    }
 }
