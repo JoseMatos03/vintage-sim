29478e4fb66c8eece02c10f4ba67e4ed9dcbed3b
JoseMatos03 <joser.fmatos03@gmail.com>
2023-04-17 21:05:27 +0100

    refac(utilizadores)

--- src/vintage/utilizadores/Utilizador.java
+++ src/vintage/utilizadores/Utilizador.java
@@ -8,39 +8,38 @@ import java.util.List;
 public class Utilizador {
 
     private int codigo; // TODO Atribuir codigo de forma automática
-
     private String email;
-
     private String nome;
-
     private String morada;
-
     private int numeroFiscal;
 
-    //TODO adicionar e remover artigos nas listas
+    // TODO adicionar e remover artigos nas listas
+    // Listas de artigos associados ao utilizador
     private List<Artigo> listados;
     private List<Artigo> vendidos;
     private List<Artigo> comprados;
 
-    //TODO quando remove do listado para o vendido, também sobe o valorEmVendas
     private float valorEmVendas;
 
-
-    public Utilizador(int codigo, String email, String nome, String morada, int numero_fiscal, float valorEmVendas)
-    {
+    public Utilizador(int codigo, String email, String nome, String morada, int numeroFiscal) {
         this.codigo = codigo;
-        this.email  = email;
+        this.email = email;
         this.nome = nome;
         this.morada = morada;
-        this.numeroFiscal = numero_fiscal;
-        this.listados = new ArrayList<>();
-        this.vendidos = new ArrayList<>();
-        this.comprados = new ArrayList<>();
-        this.valorEmVendas = valorEmVendas;
+        this.numeroFiscal = numeroFiscal;
+
+        this.listados = new ArrayList<Artigo>();
+        this.vendidos = new ArrayList<Artigo>();
+        this.comprados = new ArrayList<Artigo>();
+        this.valorEmVendas = 0;
     }
 
-    public int getCodigo(){
+    public int getCodigo() {
         return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
     }
 
     public String getEmail() {
@@ -59,44 +58,44 @@ public class Utilizador {
         this.nome = nome;
     }
 
-    public String getMorada(){
+    public String getMorada() {
         return morada;
-    }
-
-    public List<Artigo> getListados() {
-        return listados;
-    }
-
-    public List<Artigo> getVendidos() {
-        return vendidos;
-    }
-
-    public List<Artigo> getComprados() {
-        return comprados;
-    }
-
-    public void setListados(List<Artigo> listados) {
-        this.listados = listados;
-    }
-
-    public void setVendidos(List<Artigo> vendidos) {
-        this.vendidos = vendidos;
-    }
-
-    public void setComprados(List<Artigo> comprados) {
-        this.comprados = comprados;
     }
 
     public void setMorada(String morada) {
         this.morada = morada;
     }
 
-    public int getNumeroFiscal(){
+    public int getNumeroFiscal() {
         return numeroFiscal;
     }
 
     public void setNumeroFiscal(int numeroFiscal) {
         this.numeroFiscal = numeroFiscal;
+    }
+
+    public List<Artigo> getListados() {
+        return listados;
+    }
+
+    public void setListados(List<Artigo> listados) {
+        this.listados = listados;
+    }
+
+    public List<Artigo> getVendidos() {
+        return vendidos;
+    }
+
+    public void setVendidos(List<Artigo> vendidos) {
+        this.vendidos = vendidos;
+    }
+
+    public List<Artigo> getComprados() {
+        return comprados;
+    }
+
+    public void setComprados(List<Artigo> comprados) {
+        this.comprados = comprados;
     }
 
     public float getValorEmVendas() {
@@ -106,4 +105,5 @@ public class Utilizador {
     public void setValorEmVendas(float valorEmVendas) {
         this.valorEmVendas = valorEmVendas;
     }
+
 }
