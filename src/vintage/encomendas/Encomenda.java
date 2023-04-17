050b3a7591fe0b6c3207f6660ae5287c5dce9b1f
JoseMatos03 <joser.fmatos03@gmail.com>
2023-04-17 21:22:43 +0100

    refactor(encomandas): Várias mudanças.
    Adicionar/Remover artigos: Mudança para utilizar gets e sets (mais seguro).
    Preços: Mudança de double para float.
    Construtor: Retirado criação explicita duma ArrayList.
    Construtor: LocalDate para LocalDateTime para incluir tempo em horas.

--- src/vintage/encomendas/Encomenda.java
+++ src/vintage/encomendas/Encomenda.java
@@ -1,80 +1,103 @@
 package vintage.encomendas;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
+import java.time.LocalDateTime;
 import java.util.List;
 import vintage.artigos.Artigo;
 
 public class Encomenda {
-    private List<Artigo> artigos;
 
+    // Tamanho da encomenda
     public static final int GRANDE = 0;
     public static final int MEDIO = 1;
     public static final int PEQUENO = 2;
 
+    // Estado da encomenda
     public static final int PENDENTE = 0;
     public static final int EXPEDIDA = 1;
     public static final int FINALIZADA = 2;
 
+    private List<Artigo> artigos;
     private int dimensaoEncomenda;
     private int estadoEncomenda;
 
-    private double precoFinal;
-    private LocalDate dataCriacao;
+    private float precoEncomenda;
+    private LocalDateTime dataCriacao;
 
-    public Encomenda(List<Artigo> artigos)
-    {
-        this.artigos = new ArrayList<>(artigos);
-        this.dimensaoEncomenda = artigos.size();
-        this.estadoEncomenda = PENDENTE; // TODO Atualizar estado da encomenda quando for implementadp tempo
-        this.precoFinal = this.calcularPrecoFinal();
-        this.dataCriacao = LocalDate.now();
+    public void adicionarArtigos(Artigo artigo) {
+        this.getArtigos().add(artigo);  
+        float novoPrecoEncomenda = this.getPrecoEncomenda() + artigo.calcularPreco();
+        this.setPrecoEncomenda(novoPrecoEncomenda);
+        // TODO Analisar tamanho da encomenda
     }
 
-    public int getDimensao()
-    {
-        return this.dimensaoEncomenda;
+    public void removerArtigo(Artigo artigo) {
+        this.getArtigos().remove(artigo);  
+        float novoPrecoEncomenda = this.getPrecoEncomenda() - artigo.calcularPreco();
+        this.setPrecoEncomenda(novoPrecoEncomenda);
+        // TODO Analisar tamanho da encomenda
     }
 
-    public int getEstado()
-    {
-        return this.estadoEncomenda;
-    }
-    public void setEstado(int estado)
-    {
-        this.estadoEncomenda = estado;
+    public float calcularPrecoFinal() {
+        float precoEncomenda = 0;
+
+        for (int i = 0; i < this.getArtigos().size(); i++) {
+            precoEncomenda += this.getArtigos().get(i).calcularPreco();
+        }
+
+        return precoEncomenda;
     }
 
-    public LocalDate getDataCriacao()
+    // TODO Reembolso após funcionalidade de tempo implementada
+    public float reembolso() 
     {
+        return 0;
+    }
+    
+    public Encomenda(List<Artigo> artigos, int dimensaoEncomenda) {
+        this.artigos = artigos;
+        this.dimensaoEncomenda = dimensaoEncomenda;
+        this.estadoEncomenda = PENDENTE; // TODO Atualizar estado da encomenda conforme passagem de tempo
+        this.precoEncomenda = this.calcularPrecoFinal();
+        this.dataCriacao = LocalDateTime.now();
+    }
+
+    public List<Artigo> getArtigos() {
+        return artigos;
+    }
+
+    public void setArtigos(List<Artigo> artigos) {
+        this.artigos = artigos;
+    }
+
+    public int getDimensaoEncomenda() {
+        return dimensaoEncomenda;
+    }
+
+    public void setDimensaoEncomenda(int dimensaoEncomenda) {
+        this.dimensaoEncomenda = dimensaoEncomenda;
+    }
+
+    public int getEstadoEncomenda() {
+        return estadoEncomenda;
+    }
+
+    public void setEstadoEncomenda(int estadoEncomenda) {
+        this.estadoEncomenda = estadoEncomenda;
+    }
+
+    public float getPrecoEncomenda() {
+        return precoEncomenda;
+    }
+
+    public void setPrecoEncomenda(float precoFinal) {
+        this.precoEncomenda = precoFinal;
+    }
+
+    public LocalDateTime getDataCriacao() {
         return dataCriacao;
     }
 
-    public void adicionarArtigos(Artigo artigo)
-    {
-        this.artigos.add(artigo);
-        this.precoFinal += artigo.calcularPreco();
-        dimensaoEncomenda++;
-    }
-
-    public void removerArtigos(Artigo artigo)
-    {
-        this.artigos.remove(artigo);
-        this.precoFinal -= artigo.calcularPreco();
-        dimensaoEncomenda--;
-    }
-
-    public double calcularPrecoFinal()
-    {
-        double precoFinal = 0;
-        for (int i = 0; i < this.dimensaoEncomenda; i++) {
-            precoFinal += artigos.get(i).calcularPreco();
-        }
-        return precoFinal;
-    }
-
-    public double reembolso() // TODO Reembolso quando houver tempo a funcionar
-    {
-        return this.precoFinal;
+    public void setDataCriacao(LocalDateTime dataCriacao) {
+        this.dataCriacao = dataCriacao;
     }
 }
