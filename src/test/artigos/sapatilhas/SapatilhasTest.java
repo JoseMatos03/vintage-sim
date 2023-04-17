87207c692be0db52b766d3b9c3ec12c1799355be
JoseMatos03 <joser.fmatos03@gmail.com>
2023-04-17 11:44:30 +0100

    fix(testes-artigos): ponto-virgula extra

--- src/test/artigos/sapatilhas/SapatilhasTest.java
+++ src/test/artigos/sapatilhas/SapatilhasTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 
 import vintage.artigos.sapatilhas.Sapatilhas;
 import static vintage.artigos.sapatilhas.Sapatilhas.ATACADORES;
-import static vintage.artigos.sapatilhas.Sapatilhas.ATILHOS;;
+import static vintage.artigos.sapatilhas.Sapatilhas.ATILHOS;
 
 public class SapatilhasTest {
 
