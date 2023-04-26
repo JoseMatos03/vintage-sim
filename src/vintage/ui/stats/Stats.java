f08bdb15a75ef9910cdeb8b0b612e1a407bec588
Pedrosilva03 <pedroo.silva180@gmail.com>
2023-04-26 21:02:35 +0100

    feat(estatísticas): Primeira implementação de algumas estatísticas.

--- src/vintage/ui/stats/Stats.java
+++ src/vintage/ui/stats/Stats.java
@@ -1,5 +1,55 @@
 package vintage.ui.stats;
 
+import java.util.List;
+
+import vintage.Vintage;
+import vintage.utilizadores.Utilizador;
+import vintage.transportadoras.Transportadora;
+import vintage.encomendas.Encomenda;
+
 public class Stats {
+
+    public static int vendedorMaiorFaturacao(Vintage vintage) // TODO Implementar intervalos de tempo
+    {
+        int codigoUtilizador = 0;
+        float valorVendas = 0;
+        List<Utilizador> utilizadores = vintage.getUtilizadores();
+        for(Utilizador utilizador : utilizadores)
+        {
+            float valorVendasAtual = utilizador.getValorEmVendas();
+            if (valorVendasAtual > valorVendas) {
+                valorVendas = valorVendasAtual;
+                codigoUtilizador = utilizador.getCodigo();
+            }
+        }
+        return codigoUtilizador;
+    }
     
+    public static String transportadoraMaiorFaturacao(Vintage vintage)
+    {
+        String transportadoraMaior = "";
+        float maiorFaturacao = 0;
+        List<Transportadora> transportadoras = vintage.getTransportadoras();
+        for(Transportadora transportadora : transportadoras)
+        {
+            float valorExpedicao = transportadora.getValorExpedicao();
+            if (valorExpedicao > maiorFaturacao) {
+                maiorFaturacao = valorExpedicao;
+                transportadoraMaior = transportadora.getNome();
+            }
+        }
+        return transportadoraMaior;
+    }
+
+    // TODO Listar encomendas emitidas por um vendedor
+
+    public static float totalGanho(Vintage vintage)
+    {
+        List<Encomenda> encomendas = vintage.getEncomendas();
+        float ganhos = 0;
+        for (Encomenda encomenda : encomendas) {
+            ganhos += encomenda.getPrecoEncomenda();
+        }
+        return ganhos;
+    }
 }
